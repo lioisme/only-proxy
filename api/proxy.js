d90feeca8000062dc606085608be1b8df52be962
lioisme <lioisme@outlook.com>
2026-02-28 01:27:24 +0800

    更新 proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://cn.pornhub.com/";//your website url
+  let target = "http://lioisme.us.ci:8096/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
